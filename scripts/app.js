16400e1979f6b7cf15efa49f9a8ad3a6933c451a
davidbdeath <social_davidbdyer@icloud.com>
2021-11-03 02:38:49 +0000

    initial framework

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+const memIcons = [
+	{name:'astronaut', path:'./images/mem-icons/astronaut.svg'},
+	{name:'atom', path:'./images/mem-icons/atom.svg'},
+	{name:'meteor', path:'./images/mem-icons/'},
+	{name:'moon', path:'./images/mem-icons/moon.svg'},
+	{name:'robot', path:'./images/mem-icons/robot.svg'},
+	{name:'satellite dish', path:'./images/mem-icons/satellite-dish.svg'},
+	{name:'satellite', path:'./images/mem-icons/satellite.svg'},
+	{name:'space shuttle', path:'./images/mem-icons/space-shuttle.svg'},
+	{name:'spock', path:'./images/mem-icons/spock.svg'}
+]
+
+const menNumbers = [
+
+]
